bda6e9eaee2a43695a3689d15d3d9c4f20d123ed
Awad Sharif <awadsharif9@gmail.com>
2021-10-25 23:55:00 -0700

    added openweather API to scripts.js

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,9 +1,8 @@
-var cityValue = document.querySelector("#city-weather-search-value");
-var submitButton = document.querySelector("#city-weather-search-button");
+var cityName = document.querySelector("#city-weather-search-value");
+var cityForm = document.querySelector("#city-weather-form");
 var previousCityResults = document.querySelector(".city-weather-previous-search");
 var citySearchResults = document.querySelector(".city-weather-result");
 var fiveDayForecast = document.querySelector(".city-weather-5-day-forecast");
-
 // user types in a city in the search box and when user hits submit, store the city in local storage
 // when user types in the city, user should see the following:
     // the city name with today's weather, temp, wind, humidity, uv index
@@ -18,30 +17,45 @@ var fiveDayForecast = document.querySelector(".city-weather-5-day-forecast");
 
 
 // function handles submissions when user enters city and hits submit
+console.log(cityName.value)
+
 function submitCityData(event) {
     event.preventDefault();
-    var cityName = cityValue.value().trim();
     if (cityName) {
+        var cityValue = cityName.value.trim().replace(" ", '');
+        console.log(cityValue);
         getCityData(cityName);
-        cityValue = '';
+        cityName.value = '';
     }
     else {
-        window.alert("Invalid input!");
+        window.alert("City does not exist!");
     }
 }
 
 // function retrives actual weather data based off city user inputted
 function getCityData(city) {
     var apiKey = '8aaaf47fd1335c41e91dc5418eca16e9';
-    var openWeatherURL = `https://api.openweathermap.org/data/2.5/onecall?q=${city}&APPID=${apiKey}`;
-    console.log("CITY!!!!")
+    var openWeatherURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + city.value + '&appid=' + apiKey + '&units=imperial';
+    console.log(openWeatherURL);
     fetch(openWeatherURL)
         .then(function(response){
-            if (response.ok) {
+            if(response.ok) {
                 console.log(response.json);
+                response.json().then(function(data) {
+                    console.log(data);
+                })
+            }
+
+            else {
+                window.alert("Please enter a valid city name!")
             }
         })
+
+
+        .catch(function (error) {
+            alert('Please enter a city name!');
+        });
 }
 
-submitButton.addEventListener("submit", submitCityData);
+cityForm.addEventListener("submit", submitCityData);
 
